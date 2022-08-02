dee8f351e573175f42d53a15c67936c26e1a46e3
Gimena Riveros <gimena1001riveros@gmail.com>
2022-08-02 20:07:49 -0300

    Completo el menu responsivo con html5 - css3 - js. MGR

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const ham = document.querySelector('.ham');
+const links = document.querySelector('.menu-links');
+const bars = document.querySelectorAll('.ham span');
+
+
+ham.addEventListener('click', () => {
+    links.classList.toggle('activate');
+    bars.forEach(child => {child.classList.toggle('animated')});
+});
